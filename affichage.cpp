16dee3b491dd05c61874069cf6ea4716eec82028
typlosion14 <48457254+typlosion14@users.noreply.github.com>
2020-03-07 16:56:20 +0100

    Update affichage.cpp

--- affichage.cpp
+++ affichage.cpp
@@ -19,6 +19,37 @@ int main() {
     printf("L'aileron est \x85 %d%c d'usure. \n", car.aileron.Getusure(), 37);
     printf("La suspension est \x85 %d%c d'usure. \n", car.suspension.Getusure(), 37);
     printf("La batterie est \x85 %d%c de charge. \n", car.batterie.Getcharge(), 37);
+    int r = 0;
+    while (r != 9) {
+        printf("1. Remplir l'huile du moteur.\n");
+        printf("2. Refroidir le moteur.\n");
+        printf("3. Changer les pneus.\n");
+        printf("4. Remplir le reservoir.\n");
+        printf("5. R%cparer le turbo.\n", 130);
+        printf("6. R%cparer l'aileron.\n", 130);
+        printf("7. R%cparer la suspension.\n", 130);
+        printf("8. Recharger la batterie.\n");
+        printf("9. Repartir.\n");
+        cin >> r;
+        switch (r) {
+        case 1:;
+            break;
+        case 2:;
+            break;
+        case 3:;
+            break;
+        case 4:;
+            break;
+        case 5:;
+            break;
+        case 6:;
+            break;
+        case 7:;
+            break;
+        case 8:;
+            break;
+        }
+    };
 
 
 
