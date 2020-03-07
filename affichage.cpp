10b7d0d14d69b72d6871b0c9e77466f420c0c4d0
typlosion14 <48457254+typlosion14@users.noreply.github.com>
2020-03-07 19:09:03 +0100

    Ajout du temps pour le changement

--- affichage.cpp
+++ affichage.cpp
@@ -1,26 +1,30 @@
 #include <stdio.h>
 #include <iostream>
 #include "main.cpp"
+#include <string>
 using namespace std;
+Time time;
+Car car;
 int main() {
-    Car car;
-    printf("L'huile du moteur est \x85 %d%c \n",car.moteur.Getniveau_huile(), 37);
-    printf("Le moteur est \x85 %d%cC \n", car.moteur.Gettemperature(), 248);//max 900
-    printf("Le pneu avant gauche est \x85 %d%c d'usure.\n", car.pneu_avant_gauche.Getusure(),37);
-    printf("Le pneu avant droit est \x85 %d%c d'usure.\n", car.pneu_avant_droit.Getusure(), 37);
-    printf("Le pneu arri%cre gauche est \x85 %d%c d'usure. \n", 138, car.pneu_arriere_gauche.Getusure(), 37);
-    printf("Le pneu arri%cre droit est \x85 %d%c d'usure. \n", 138, car.pneu_arriere_droit.Getusure(), 37);
-    printf("La pression du pneu avant gauche est \x85 %d%c de pression.\n", car.pneu_avant_gauche.Getpression(), 37);//Pression
-    printf("La pression du pneu avant droit est \x85 %d%c de pression.\n", car.pneu_avant_droit.Getpression(), 37);
-    printf("La pression du pneu arri%cre droit est \x85 %d%c de pression.\n", 138, car.pneu_arriere_droit.Getpression(), 37);
-    printf("La pression du pneu arri%cre gauche est \x85 %d%c de pression.\n",138, car.pneu_arriere_gauche.Getpression(), 37);
-    printf("Le reservoir est \x85 %d%c. \n", car.reservoir.Getniveau_essence(), 37);
-    printf("Le turbo est \x85 %d%c d'usure. \n", car.turbo.Getusure(), 37);
-    printf("L'aileron est \x85 %d%c d'usure. \n", car.aileron.Getusure(), 37);
-    printf("La suspension est \x85 %d%c d'usure. \n", car.suspension.Getusure(), 37);
-    printf("La batterie est \x85 %d%c de charge. \n", car.batterie.Getcharge(), 37);
     int r = 0;
     while (r != 9) {
+        printf("Temps pass%c:", 130);
+        cout << time.Gettime() << endl;
+        printf("L'huile du moteur est \x85 %d%c \n", car.moteur.Gethuile(), 37);
+        printf("Le moteur est \x85 %d%cC \n", car.moteur.Gettemperature(), 248);//max 900
+        printf("Le pneu avant gauche est \x85 %d%c d'usure.\n", car.pneu_avant_gauche.Getusure(), 37);
+        printf("Le pneu avant droit est \x85 %d%c d'usure.\n", car.pneu_avant_droit.Getusure(), 37);
+        printf("Le pneu arri%cre gauche est \x85 %d%c d'usure. \n", 138, car.pneu_arriere_gauche.Getusure(), 37);
+        printf("Le pneu arri%cre droit est \x85 %d%c d'usure. \n", 138, car.pneu_arriere_droit.Getusure(), 37);
+        printf("La pression du pneu avant gauche est \x85 %d%c de pression.\n", car.pneu_avant_gauche.Getpression(), 37);//Pression
+        printf("La pression du pneu avant droit est \x85 %d%c de pression.\n", car.pneu_avant_droit.Getpression(), 37);
+        printf("La pression du pneu arri%cre droit est \x85 %d%c de pression.\n", 138, car.pneu_arriere_droit.Getpression(), 37);
+        printf("La pression du pneu arri%cre gauche est \x85 %d%c de pression.\n", 138, car.pneu_arriere_gauche.Getpression(), 37);
+        printf("Le reservoir est \x85 %d%c. \n", car.reservoir.Getessence(), 37);
+        printf("Le turbo est \x85 %d%c d'usure. \n", car.turbo.Getusure(), 37);
+        printf("L'aileron est \x85 %d%c d'usure. \n", car.aileron.Getusure(), 37);
+        printf("La suspension est \x85 %d%c d'usure. \n", car.suspension.Getusure(), 37);
+        printf("La batterie est \x85 %d%c de charge. \n", car.batterie.Getcharge(), 37);
         printf("1. Remplir l'huile du moteur.\n");
         printf("2. Refroidir le moteur.\n");
         printf("3. Changer les pneus.\n");
@@ -36,25 +40,32 @@ int main() {
             r = 0;
         }
         switch (r) {
-        case 1:;
+        case 1:car.moteur.Refill();
+            time.Updatetime(car.moteur.Gettime());
             break;
-        case 2:;
+        case 2:car.moteur.Settemperature(car.moteur.Gettemperature()-10);
+            time.Updatetime(car.moteur.Gettime());
             break;
-        case 3:;
+        case 3:car.Change_pneu();
+            time.Updatetime(car.pneu_arriere_droit.Gettime());
             break;
-        case 4:;
+        case 4:car.reservoir.Refill();
+            time.Updatetime(car.reservoir.Gettime());
             break;
-        case 5:;
+        case 5:car.turbo.Change();
+            time.Updatetime(car.turbo.Gettime());
             break;
-        case 6:;
+        case 6:car.aileron.Change();
+            time.Updatetime(car.aileron.Gettime());
             break;
-        case 7:;
+        case 7:car.suspension.Change();
+            time.Updatetime(car.suspension.Gettime());
             break;
-        case 8:;
+        case 8:car.batterie.Change();
+            time.Updatetime(car.batterie.Gettime());
             break;
         }
     };
-
 
 
     return 0;
