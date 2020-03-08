9484322b39b33c260721ef5a6f5d410a125fea3c
typlosion14 <48457254+typlosion14@users.noreply.github.com>
2020-03-08 16:32:46 +0100

    Genere des float random
    Par gonzague

---
+++ random.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+
+
+float random (int start, int end){
+    srand (time(NULL));
+    int number = rand() % end + start;
+    float decimal =(rand()%100 +1);
+    decimal=decimal/100;
+    float random_value= number+decimal;
+    
+    return random_value;
+}
+
